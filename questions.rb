cc217ac4f6e279658ca8d6cc08f7a9ac9f8336d9
Christopher Haines <hainesy91@gmail.com>
2015-05-19 19:33:55 +0100

    answered the 9th questions

--- questions.rb
+++ questions.rb
@@ -46,7 +46,7 @@ end
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
-  
+  array.sort_by { |nature| nature.slice(-1)}
 end
 
 # cut strings in half, and return the first half, e.g.
