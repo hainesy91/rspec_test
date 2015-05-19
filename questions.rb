dd73b3d228f74229ad7abfb339d18e804811a763
Christopher Haines <hainesy91@gmail.com>
2015-05-19 18:31:33 +0100

    solved the second test

--- questions.rb
+++ questions.rb
@@ -5,7 +5,7 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
-  
+  array.select { |names| names[0].downcase == 'a' || names[0].downcase == 'o' || names[0].downcase == 'i'}
 end
 
 # remove instances of nil (but NOT false) from an array
