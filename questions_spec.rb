3cbf04571bf7277955e78fc97bef9cfb8ab7a80c
Christopher Haines <hainesy91@gmail.com>
2015-05-20 08:52:10 +0100

    solved the test on line 74

--- questions_spec.rb
+++ questions_spec.rb
@@ -71,12 +71,12 @@ describe 'Ruby Homework' do
     expect(n).to eq [[2, 4, 6], [1, 3, 5, 7]]
   end
 
-  fit 'number_of_elements_that_are_palindromes' do
+  it 'number_of_elements_that_are_palindromes' do
     n = number_of_elements_that_are_palindromes ['bob', 'radar', 'alex', 'noon', 'banana']
     expect(n).to eq 3
   end
 
-  it 'shortest_word_in_array' do
+  fit 'shortest_word_in_array' do
     n = shortest_word_in_array %w(here is a bunch of words of different lengths)
     expect(n).to eq 'a'
   end
